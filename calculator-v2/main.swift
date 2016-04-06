e5451077a5b5fce6220389ca0dbb0e19af3e9191
Josh Hall <joshhall9@hotmail.com>
2016-04-05 22:39:54 -0700

    Initial Commit

---
+++ calculator-v2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calculator-v2
+//
+//  Created by Joshua Hall on 4/5/16.
+//  Copyright © 2016 Joshua Hall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
